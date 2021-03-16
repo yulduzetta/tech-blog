a40819603dbeca9ccae3211dd9e1d7ca8f8b0bb1
yulduz <yulduz83@gmail.com>
2021-03-16 05:35:48 -0500

    comment ccss

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,7 +2,7 @@ async function commentFormHandler(event) {
     event.preventDefault();
   
     const comment_text = document
-      .querySelector("textarea[name=comment-body]")
+      .querySelector("#content")
       .value.trim();
   
     const post_id = window.location.toString().split("/")[
